e171dc715a454c3332c647be63fe38d4d0f5f175
kumarnabin <touchnabin@gmail.com>
2025-01-01 12:03:06 +0545

    database feature added

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 use Konnect\NayaFramework\Lib\Route;
 
 require_once '../vendor/autoload.php';
-require_once '../helper/helper.php';
+require_once '../config/main.php';
 
 Route::run();
--- src/Lib/Request.php
+++ src/Lib/Request.php
@@ -4,69 +4,78 @@ namespace Konnect\NayaFramework\Lib;
 
 class Request
 {
-    // Store GET, POST, and raw JSON data
     private array $get;
     private array $post;
     private array $json;
+    private array $files;
 
     public function __construct()
     {
-        // Initialize GET and POST data
         $this->get = $_GET;
         $this->post = $_POST;
         $this->json = [];
+        $this->files = $_FILES;
 
-        // Parse incoming JSON data if the content-type is application/json
         if ($this->isJsonRequest()) {
             $this->json = json_decode(file_get_contents('php://input'), true) ?? [];
         }
     }
 
-    // Check if the request has JSON data
     private function isJsonRequest(): bool
     {
         return isset($_SERVER['CONTENT_TYPE']) && str_contains($_SERVER['CONTENT_TYPE'], 'application/json');
     }
 
-    // Get a parameter from the GET request
     public function get($key, $default = null)
     {
         return $this->get[$key] ?? $default;
     }
 
-    // Get a parameter from the POST request
     public function post($key, $default = null)
     {
         return $this->post[$key] ?? $default;
     }
 
-    // Get a parameter from the JSON request
     public function json($key, $default = null)
     {
         return $this->json[$key] ?? $default;
     }
 
-    // Get all GET parameters
     public function allGet(): array
     {
         return $this->get;
     }
 
-    // Get all POST parameters
     public function allPost(): array
     {
         return $this->post;
     }
 
-    // Get all parameters (GET, POST, and JSON merged)
     public function all(): array
     {
         return array_merge($this->get, $this->post, $this->json);
     }
 
-    // Get a parameter from either GET, POST, or JSON
     public function take($key)
     {
         return $this->json[$key] ?? $this->post[$key] ?? $this->get[$key] ?? null;
+    }
+
+    // Check if a file is present in the request
+    public function hasFile(string $key): bool
+    {
+        return isset($this->files[$key]) && $this->files[$key]['error'] === UPLOAD_ERR_OK;
+    }
+
+    // Get details of a specific uploaded file
+    public function file(string $key): ?array
+    {
+        return $this->hasFile($key) ? $this->files[$key] : null;
+    }
+
+    // Get all uploaded files
+    public function allFiles(): array
+    {
+        return $this->files;
     }
 }
