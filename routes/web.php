7ab274b2edfa3883b0c23ad978b1ae7a45a795c8
kumarnabin <touchnabin@gmail.com>
2025-01-21 21:20:43 +0545

    twig

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,6 @@ use Konnect\NayaFramework\Lib\Route;
 // Define routes
 Route::get('/', [IndexController::class, 'index']);
 // Define routes
-Route::get('/cc', function () {
-    try {
-        if (!file_exists(__DIR__ . '/../../cache')) {
-            mkdir(__DIR__ . '/../../cache', 0777, true);
-        }
-        if (file_exists(__DIR__ . '/../../cache/routes.php')) {
-            unlink(__DIR__ . '/../../cache/routes.php');
-            echo "Cache cleared!";
-        } else {
-            echo "Cache not found!";
-        }
-    } catch (Exception $e) {
-        echo "Error clearing cache: " . $e->getMessage();
-    }
-});
 
 Route::get('/refresh-users', [UserController::class, 'refresh']);
 Route::resource('/users', UserController::class);
